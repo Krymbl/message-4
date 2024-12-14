62ad8f104b8895255af417694314d6ee5972d777
Kamil Sabirov <Sabirovafarida.kamil@gmail.com>
2024-12-14 13:59:43 +0300

    Написал классы User и UserService

--- src/main/java/Message14/User.java
+++ src/main/java/Message14/User.java
@@ -1,13 +1,55 @@
 package Message14;
 
-/*
-    - ник найм
-    - имя пользователя
-    - телефон
-    - пароль
-*/
-
-
 public class User {
+    private String nickname;
+    private String name;
+    private String number;
+    private String password;
+
+    public User() {}
+
+    public User(String name, String nickname, String password, String number) {
+        this.password = password;
+        this.number = number;
+        this.nickname = nickname;
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "Имя пользователя: " + name + " / Ник: " + nickname + " / Номер: " + number;
+    }
+
 
 }
--- src/main/java/Message14/UserService.java
+++ src/main/java/Message14/UserService.java
@@ -1,5 +1,24 @@
 package Message14;
 
 public class UserService {
+    private User[] usersArray = new User[4];
+
+    public void create4Users(){
+
+        usersArray[0] = new User("Диана", "DiVilisova", "LoveLoveluv782", "+79393373236");
+        usersArray[1] = new User("Евгений", "fourteenJule", "MusiC1407", "+79172823129" );
+        usersArray[2] = new User("Риана", "rioriosol", "Kolipaiol", "+79073264412");
+        usersArray[3] = new User("Давид", "Crumbl", "Jokitusi", "+79023254639");
+    }
+
+    public User[] getUsersArray() {
+        return usersArray;
+    }
+
+    public void printUsers() {
+        for (User user: usersArray){
+            System.out.println(user);
+        }
+    }
 
 }
